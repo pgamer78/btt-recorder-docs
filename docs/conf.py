a5751543878a7330a7643308029dfea75aa811ae
Craig Oliver <craig.robert.oliver@gmail.com>
2019-11-01 12:07:27 -0400

    Fix bug: Recorder, not Writer

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'BTT Writer Information'
+project = u'BTT Recorder Information'
 copyright = u'2017, Door43'
 author = u'Door43'
 
@@ -136,7 +136,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
 #
-# html_title = u'BTT Writer Information v0.1'
+# html_title = u'BTT Recorder Information v0.1'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #
@@ -236,7 +236,7 @@ html_static_path = ['_static']
 # html_search_scorer = 'scorer.js'
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'BTTWriter'
+htmlhelp_basename = 'BTTRecorder'
 
 # -- Options for LaTeX output ---------------------------------------------
 
@@ -262,7 +262,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'BTTWriter.tex', u'BTT Writer Information',
+    (master_doc, 'BTTRecorder.tex', u'BTT Recorder Information',
      u'Door43', 'manual'),
 ]
 
@@ -304,7 +304,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'unlockedbiblenotesinfo', u'BTT Writer Information',
+    (master_doc, 'unlockedbiblenotesinfo', u'BTT Recorder Information',
      [author], 1)
 ]
 
@@ -319,8 +319,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'BTTWriter', u'BTT Writer Information',
-     author, 'BTTWriter', 'One line description of project.',
+    (master_doc, 'BTTRecorder', u'BTT Recorder Information',
+     author, 'BTTRecorder', 'One line description of project.',
      'Miscellaneous'),
 ]
 
